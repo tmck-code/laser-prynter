360fce7dc9de053ebf46e2a62738edaec9d4efbc
Tom McKeesick <tmck01@gmail.com>
2026-02-10 01:49:34 +1100

    update

--- laser_prynter/pbar.py
+++ laser_prynter/pbar.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+from random import randint
 import shutil
 import time
+from typing import NamedTuple
 
 from laser_prynter.colour.gradient import interp_xyz
 
@@ -9,11 +11,19 @@ def indexes(t, w):
     for i in range(max(t+1,w)):
         yield int(i*(max(w,t)/min(w,t)))
 
+class RGB(NamedTuple):
+    r: int
+    g: int
+    b: int
+
+DEFAULT_C1, DEFAULT_C2 = RGB(240,50,0), RGB(10,220,0)
+
+
 class PBar:
-    def __init__(self, total):
+    def __init__(self, total: int, c1: RGB=DEFAULT_C1, c2: RGB=DEFAULT_C2):
         self.t = total
         self.w = shutil.get_terminal_size().columns
-        self.curr, self.g = 0, interp_xyz((240,50,0),(10,220,0), max(self.t+1, self.w))
+        self.curr, self.g = 0, interp_xyz(c1, c2, max(self.t+1, self.w))
         self._iter_pbar = iter(self._pbar())
 
 
@@ -28,5 +38,28 @@ class PBar:
         if self.curr >= max(self.t, self.w):
             raise StopIteration
         for i, (r,g,b) in next(self._iter_pbar):
-            print(f'\x1b[38;2;{int(r)};{int(g)};{int(b)}mN\x1b[0m', end='', flush=True)
+            print(f'\x1b[38;2;{int(r)};{int(g)};{int(b)}m▉\x1b[0m', end='', flush=True)
             self.curr = i
+
+    def update(self, n: int):
+        for _ in range(n):
+            next(self)
+
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.curr = self.t
+
+
+if __name__ == '__main__':
+    import time
+    c1, c2 = RGB(randint(0,255),randint(0,255),randint(0,255)), RGB(randint(0,255),randint(0,255),randint(0,255))
+    # for i in PBar(100, c1, c2):
+    #     time.sleep(0.05)
+    with PBar(100, c1, c2) as pbar:
+        for i in range(100):
+            pbar.update(1)
+            time.sleep(0.05)
+

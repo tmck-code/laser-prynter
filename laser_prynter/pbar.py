ed8bea5ecb7a67faaf8757b60e21a386085b99db
Tom McKeesick <tmck01@gmail.com>
2026-02-18 08:57:27 +1100

    update

--- laser_prynter/pbar.py
+++ laser_prynter/pbar.py
@@ -55,38 +55,23 @@ class PBar:
         sys.exit(0)
 
     def sigwinch_handler(self, _signum: int, _frame: types.FrameType | None) -> None:
-        self.w, self.h = _get_terminal_size()
         _print_to_terminal(
-            '\x1b7'  # save cursor position
-            f'\x1b[0;{self.h - 2}r'  # set top & bottom regions (margins) - reserve 2 lines
-            f'\x1b[{self.h};1000H'  # move to bottom line
-            '\r'
+            f'\x1b[0;{self.h - 1}r'  # set top & bottom regions (margins) - reserve 2 lines
+            f'\x1b[{self.h};0H'  # move to bottom line
             '\x1b[2K'  # clear entire line
-            f'\x1b[{self.h - 1};1000H'  # move to bottom line
-            '\r'
+            f'\x1b[{self.h};0H'  # move to bottom line
             '\x1b[2K'  # clear entire line
-            '\x1b8'  # restore cursor position
-            '\x1b[2A'  # move cursor up 2 lines
+            '\x1b[10A'  # move cursor up 2 lines
         )
+        self.w, self.h = _get_terminal_size()
+        self.g = RGBGradient(start=self.g.start, end=self.g.end, steps=self.w)
+        self._initial_bar()
 
-        # curr, progress = self.curr, self.progress
-        # self.curr, self.progress = 0, 0
-        # if self.t > self.w:
-        #     self.g = list(interp_xyz(self.c1, self.c2, self.t + 1))
-        # else:
-        #     self.g = list(interp_xyz(self.c1, self.c2, self.w + 1))
+        i = self.i
+        self.i, self.x_pos = 0, 0
+        self.update(i)
 
-        # self._iter_pbar = iter(self._pbar())
-
-        # self._initial_bar()
-        # self.progress = progress
-        # for _ in range(curr):
-        #     try:
-        #         next(self)
-        #     except StopIteration:
-        #         break
-
-        # self._print_info()
+        self._print_info()
 
     @staticmethod
     def randgrad() -> tuple[RGBColour, RGBColour]:
